8251e9ba5cefbfbe6e4c5f4ab252286dda01c425
Hafsa Khan <cub.hafsakhan@gmail.com>
2023-07-23 03:12:36 +0500

    open olx pk website

---
+++ cypress/e2e/openWebPage.cy.js
@@ -0,0 +1,8 @@
+///<reference types="cypress"/>
+
+describe("template spec", () => {
+  it("passes", () => {
+    cy.visit("https://olx.com.pk");
+    //cy.get(".gLFyf").type("{enter}");
+  });
+});
